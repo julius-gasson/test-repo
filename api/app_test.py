9af944ec044d26c10e921ce00596fead3009b000
Julius Gasson <jg2023@ic.ac.uk>
2023-10-26 18:09:54 +0100

    Added data

--- api/app_test.py
+++ api/app_test.py
@@ -1,5 +1,22 @@
 from app import app, process_query, get_score
 import pytest
+    user_answers = [
+        "Japan",
+        "Rome",
+        "Spanish",
+        "Smith",
+        "scurvy",
+        "Apollo",
+        "Al Capone",
+        "1945",
+        "x",
+        "Eminem",
+        "10080",
+        "Volkswagen",
+        "x",
+        "12",
+        "x"
+    ]
 
 
 def test_knows_about_dinosaurs():
@@ -28,27 +45,49 @@ def test_quiz_route_post(client):
     assert response.status_code == 302
 
 
-def test_submit_route_post(client):
-    response = client.post('/submit')
+def test_submit_route_post_with_sample_answers(client):
+    response = client.post('/submit', data={
+        'q1': 'Japan',
+        'q2': 'Rome',
+        'q3': 'Spanish',
+        'q4': 'Smith',
+        'q5': 'Scurvy',
+        'q6': 'Apollo',
+        'q7': 'Al Capone',
+        'q8': '1945',
+        'q9': 'Walt Disney',
+        'q10': 'Drake',
+        'q11': '10080',
+        'q12': 'Volkswagen',
+        'q13': 'Amazon',
+        'q14': '12',
+        'q15': 'Astrophysics'
+    })
     assert response.status_code == 200
 
 
+def test_submit_route_get_with_sample_answers(client):
+    response = client.get('/submit', data={
+        'q1': 'Japan',
+        'q2': 'Rome',
+        'q3': 'Spanish',
+        'q4': 'Smith',
+        'q5': 'Scurvy',
+        'q6': 'Apollo',
+        'q7': 'Al Capone',
+        'q8': '1945',
+        'q9': 'Walt Disney',
+        'q10': 'Drake',
+        'q11': '10080',
+        'q12': 'Volkswagen',
+        'q13': 'Amazon',
+        'q14': '12',
+        'q15': 'Astrophysics'
+    })
+
+    assert response.status_code == 200
+
+
+
 def score_calculated_correctly():
-    user_answers = [
-        "Japan",
-        "Rome",
-        "Spanish",
-        "Smith",
-        "scurvy",
-        "Apollo",
-        "Al Capone",
-        "1945",
-        "x",
-        "Eminem",
-        "10080",
-        "Volkswagen",
-        "x",
-        "12",
-        "x"
-    ]
     assert get_score(user_answers) == 11
