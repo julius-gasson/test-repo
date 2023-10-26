afa79d1fcdab2fac7cdf94fa961a83d363663889
Julius Gasson <jg2023@ic.ac.uk>
2023-10-26 12:39:34 +0100

    s

--- api/app.py
+++ api/app.py
@@ -49,10 +49,13 @@ def quiz():
         return redirect(url_for("submit", answers=user_answers))
     return render_template("quiz.html", questions=questions)
 
+def get_score(user_answers, answers):
+    sum(1 for user, correct in zip(user_answers, answers) if user.lower() == correct.lower())
+
 @app.route("/submit", methods=["GET", "POST"])
 def submit():
     user_answers = request.args.getlist("answers")
-    score = sum(1 for user, correct in zip(user_answers, answers) if user.lower() == correct.lower())
+    score = get_score(user_answers, answers)
     return render_template("submit.html", user_answers=user_answers, score=score)
 
 def process_query(input):
@@ -60,3 +63,10 @@ def process_query(input):
         return "Dinosaurs ruled the Earth 200 million years ago"
     if input == "asteroids":
         return "Unknown"
+
+
+@app.route("/query", methods=["GET"])
+def query():
+    query_param = request.args.get('q')
+    return process_query(query_param)
+
