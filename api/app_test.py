f4bad2139fee6fb3bd0ff410ebbbf04a0c6512fe
Julius Gasson <jg2023@ic.ac.uk>
2023-10-26 15:39:39 +0100

    fixed test file

--- api/app_test.py
+++ api/app_test.py
@@ -1,5 +1,6 @@
 from app import process_query, get_score
 
+
 def test_knows_about_dinosaurs():
     assert process_query("dinosaurs") == ("Dinosaurs ruled the Earth "
                                           "200 million years ago")
@@ -8,23 +9,23 @@ def test_knows_about_dinosaurs():
 def test_does_not_know_about_asteroids():
     assert process_query("asteroids") == "Unknown"
 
+
 def score_calculated_correctly():
     user_answers = [
-    "Japan", 
-    "Rome", 
-    "Spanish", 
-    "Smith",
-    "scurvy",
-    "Apollo",
-    "Al Capone",
-    "1945",
-    "x",
-    "Eminem",
-    "10080", 
-    "Volkswagen",
-    "x",
-    "12",
-    "x"
+        "Japan",
+        "Rome",
+        "Spanish",
+        "Smith",
+        "scurvy",
+        "Apollo",
+        "Al Capone",
+        "1945",
+        "x",
+        "Eminem",
+        "10080",
+        "Volkswagen",
+        "x",
+        "12",
+        "x"
     ]
     assert get_score(user_answers) == 11
-    
